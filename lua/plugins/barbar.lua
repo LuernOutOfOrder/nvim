e89097fc88eb2b601a37e0d77f8537ba1bdbdd88
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-05-03 19:40:22 +0200

    feat: update config for barbar

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,8 +1,15 @@
 return {
   "romgrk/barbar.nvim",
   config = function()
+    require("barbar").setup({
+      animation = true,
+
+      -- Automatically hide the tabline when there are this many buffers left.
+      -- Set to any value >=0 to enable.
+      auto_hide = true,
+    })
   end,
-  vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferPrevious<CR>', {}),
-  vim.keymap.set('n', '<Tab>', '<Cmd>BufferNext<CR>', {}),
-  vim.keymap.set('n', '<Leader>x', '<Cmd>BufferClose<CR>', {}),
+  vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferPrevious<CR>", {}),
+  vim.keymap.set("n", "<Tab>", "<Cmd>BufferNext<CR>", {}),
+  vim.keymap.set("n", "<Leader>x", "<Cmd>BufferClose<CR>", {}),
 }
