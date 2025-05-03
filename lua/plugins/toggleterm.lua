95a061c3a838b52f7b12d12f0cb1aa5352be1b1b
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-05-03 22:15:35 +0200

    docs: add section to plugins and cleanup toggleterm plugin file

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,10 +1,7 @@
 return {
-	-- amongst your other plugins
-	{ "akinsho/toggleterm.nvim", version = "*", config = true },
-	-- or
-	{
 		"akinsho/toggleterm.nvim",
 		version = "*",
+    config = true,
 		opts = {
 			config = function()
 				require("toggleterm").setup({})
@@ -13,5 +10,4 @@ return {
 		vim.keymap.set("n", "<leader>th", ":ToggleTerm size=20 direction=horizontal<CR>"),
 		vim.keymap.set("n", "<leader>tv", ":ToggleTerm size=70 direction=vertical<CR>"),
 		vim.keymap.set("n", "<leader>tf", ":ToggleTerm size=70 direction=float<CR>"),
-	},
 }
