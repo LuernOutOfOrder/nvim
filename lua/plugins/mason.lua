f2cf3e5e9079c8fe5138b522267f0c413884cde7
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-05-23 22:51:23 +0200

    chore: update plugins and add zls

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -10,7 +10,7 @@ return {
     config = function()
       require("mason-lspconfig").setup({
         -- list of all LSP ensure installed
-        ensure_installed = {"lua_ls", "arduino_language_server", "asm_lsp", "clangd", "neocmake", "gopls", "jsonls", "harper_ls", "bashls"}
+        ensure_installed = {"lua_ls", "arduino_language_server", "asm_lsp", "clangd", "neocmake", "gopls", "jsonls", "harper_ls", "bashls", "zls"}
       })
     end
   }
