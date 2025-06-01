3ba5a2c016301fb5057868d9aa8cc06558e0b43c
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-06-01 19:42:25 +0200

    chore: update plugins and add automatic enabled on mason for lsp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,16 +3,27 @@ return {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
         -- list of all LSP ensure installed
-        ensure_installed = {"lua_ls", "arduino_language_server", "asm_lsp", "clangd", "neocmake", "gopls", "jsonls", "harper_ls", "bashls", "zls"}
+        automatic_enable = true,
+        ensure_installed = {
+          "lua_ls",
+          "arduino_language_server",
+          "asm_lsp",
+          "clangd",
+          "neocmake",
+          "gopls",
+          "jsonls",
+          "harper_ls",
+          "bashls",
+          "zls",
+        },
       })
-    end
-  }
+    end,
+  },
 }
-
