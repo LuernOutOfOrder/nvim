71acf13fcd842e455032c0e12a706a8c03238402
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-12-17 15:47:54 +0100

    feat: remove useless lsp, remove harper-ls config, add marksman and format buffer

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,92 +1,84 @@
 return {
-	"neovim/nvim-lspconfig",
-	config = function()
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  "neovim/nvim-lspconfig",
+  config = function()
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
     -- Definition of all servers with new api
-		vim.lsp.config["lua_ls"] = {
-			cmd = { "lua-language-server" },
-			filetypes = { "lua" },
-			capabilities = capabilities,
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" },
-					},
-				},
-			},
-		}
+    vim.lsp.config["lua_ls"] = {
+      cmd = { "lua-language-server" },
+      filetypes = { "lua" },
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+        },
+      },
+    }
 
-		vim.lsp.config["gopls"] = {
-			cmd = { "gopls" },
-			capabilities = capabilities,
-		}
+    vim.lsp.config["gopls"] = {
+      cmd = { "gopls" },
+      capabilities = capabilities,
+    }
 
-		vim.lsp.config["clangd"] = {
-			cmd = { "clangd" },
-			capabilities = capabilities,
-		}
+    vim.lsp.config["clangd"] = {
+      cmd = { "clangd" },
+      capabilities = capabilities,
+    }
 
-		vim.lsp.config["arduino_language_server"] = {
-			cmd = { "arduino_language_server" },
-			capabilities = capabilities,
-		}
+    vim.lsp.config["asm_lsp"] = {
+      cmd = { "asm-lsp" },
+      capabilities = capabilities,
+      filetypes = { "asm", "s", "S" },
+    }
 
-		vim.lsp.config["asm_lsp"] = {
-			cmd = { "asm_lsp" },
-			capabilities = capabilities,
-		}
+    vim.lsp.config["jsonls"] = {
+      cmd = { "json-languageserver", "--stdio" },
+      capabilities = capabilities,
+    }
 
-		vim.lsp.config["neocmake"] = {
-			cmd = { "neocmake" },
-			capabilities = capabilities,
-		}
+    vim.lsp.config["bashls"] = {
+      cmd = { "bash-language-server", "start" },
+      capabilities = capabilities,
+      filetypes = { "sh", "bash" },
+    }
 
-		vim.lsp.config["harper_ls"] = {
-			cmd = { "harper_ls" },
-			capabilities = capabilities,
-		}
+    vim.lsp.config["zls"] = {
+      cmd = { "zls" },
+      capabilities = capabilities,
+    }
 
-		vim.lsp.config["jsonls"] = {
-			cmd = { "json-languageserver", "--stdio" },
-			capabilities = capabilities,
-		}
+    vim.lsp.config["taplo"] = {
+      cmd = { "taplo", "lsp", "stdio" },
+      capabilities = capabilities,
+      filetypes = { "toml" },
+      root_markers = { ".git", "Cargo.toml", "taplo.toml" },
+    }
 
-		vim.lsp.config["bashls"] = {
-			cmd = { "bash-language-server", "start" },
-			capabilities = capabilities,
-			filetypes = { "sh", "bash" },
-		}
-
-		vim.lsp.config["zls"] = {
-			cmd = { "zls" },
-			capabilities = capabilities,
-		}
-
-		vim.lsp.config["taplo"] = {
-			cmd = { "taplo", "lsp" },
-			capabilities = capabilities,
-			filetypes = { "toml" },
-		}
+    vim.lsp.config.marksman = {
+      filetypes = { "markdown" },
+      root_markers = { ".git", ".marksman.toml" },
+    }
 
     -- Enable all servers
-		vim.lsp.enable({
-			"lua_ls",
-			"gopls",
-			"clangd",
-			"arduino_language_server",
-			"asm_lsp",
-			"neocmake",
-			"harper_ls",
-			"jsonls",
-			"bashls",
-			"zls",
-			"taplo",
-		})
+    vim.lsp.enable({
+      "lua_ls",
+      "asm_lsp",
+      "clangd",
+      "gopls",
+      "jsonls",
+      "harper_ls",
+      "bashls",
+      "zls",
+      "taplo",
+      "marksman",
+    })
 
-		-- Keymaps
-		vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "Display current function description (hover)" })
-		vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "Go to definition of current function" })
-		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Open code action window" })
-	end,
+    -- Keymaps
+    vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "Display current function description (hover)" })
+    vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "Go to definition of current function" })
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Open code action window" })
+    vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format current buffer using the correct LSP" })
+  end,
 }
