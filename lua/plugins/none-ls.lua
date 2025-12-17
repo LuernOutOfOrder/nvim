75ace5039a457fe38059772e72c500d17906bafa
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-12-17 15:48:14 +0100

    feat: remove useless lsp or formatter, add mardown

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,26 +1,22 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        -- misc
-        null_ls.builtins.completion.spell,
-        -- lua
-        null_ls.builtins.formatting.stylua,
-        -- go
-        null_ls.builtins.diagnostics.golangci_lint,
-        null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.goimports,
-        null_ls.builtins.formatting.goimports_reviser,
-        null_ls.builtins.formatting.golines,
-        -- rust
-        -- null_ls.builtins.formatting.rustfmt,
-        -- c, cpp
-        null_ls.builtins.formatting.uncrustify,
-      },
-    })
-    vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {desc= "Format code in the current buffer"})
-  end,
+		null_ls.setup({
+			sources = {
+				-- misc
+				null_ls.builtins.completion.spell,
+				-- rust
+				-- null_ls.builtins.formatting.rustfmt,
+				-- c, cpp
+				null_ls.builtins.formatting.uncrustify,
+				-- md
+				null_ls.builtins.formatting.markdownlint.with({
+					filetypes = { "markdown" },
+				}),
+			},
+		})
+		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format code in the current buffer" })
+	end,
 }
