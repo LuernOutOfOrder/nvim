4e9aa6a753d7907333217bbb06355d3a4a68c4d2
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2026-01-09 16:55:52 +0100

    feat: update cd keymap to jmp direct on def of a function if no multiple different choice

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -77,7 +77,39 @@ return {
 
     -- Keymaps
     vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "Display current function description (hover)" })
-    vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "Go to definition of current function" })
+    vim.keymap.set("n", "<leader>cd", function()
+      vim.lsp.buf.definition({
+        on_list = function(options)
+          local items = options.items
+          if not items or #items == 0 then
+            return
+          end
+
+          if #items == 1 then
+            vim.cmd("edit " .. items[1].filename)
+            vim.api.nvim_win_set_cursor(0, { items[1].lnum, items[1].col - 1 })
+            return
+          end
+
+          -- Check if all locations are identical
+          local first = items[1]
+          for _, item in ipairs(items) do
+            if item.filename ~= first.filename
+                or item.lnum ~= first.lnum
+                or item.col ~= first.col
+            then
+              vim.fn.setqflist(items)
+              vim.cmd("copen")
+              return
+            end
+          end
+
+          -- All identical → jump
+          vim.cmd("edit " .. first.filename)
+          vim.api.nvim_win_set_cursor(0, { first.lnum, first.col - 1 })
+        end,
+      })
+    end, { desc = "Go to definition (no useless picker)" })
     vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Open code action window" })
     vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format current buffer using the correct LSP" })
   end,
