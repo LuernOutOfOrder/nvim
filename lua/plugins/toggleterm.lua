e3a019828d820893b8a12060c8509e3ffcb75513
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2026-01-13 15:04:02 +0100

    feat: add esc keymap to escape from terminal mode easily

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,13 +1,14 @@
 return {
-		"akinsho/toggleterm.nvim",
-		version = "*",
-    config = true,
-		opts = {
-			config = function()
-				require("toggleterm").setup({})
-			end,
-		},
-		vim.keymap.set("n", "<leader>th", ":ToggleTerm size=20 direction=horizontal<CR>", {desc= "Toggle horizontal terminal"}),
-		vim.keymap.set("n", "<leader>tv", ":ToggleTerm size=70 direction=vertical<CR>", {desc= "Toggle vertical terminal"}),
-		vim.keymap.set("n", "<leader>tf", ":ToggleTerm size=70 direction=float<CR>", {desc= "Toggle floatting terminal"}),
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = true,
+  opts = {
+    config = function()
+      require("toggleterm").setup({})
+    end,
+  },
+  vim.keymap.set("n", "<leader>th", ":ToggleTerm size=20 direction=horizontal<CR>", { desc = "Toggle horizontal terminal" }),
+  vim.keymap.set("n", "<leader>tv", ":ToggleTerm size=70 direction=vertical<CR>", { desc = "Toggle vertical terminal" }),
+  vim.keymap.set("n", "<leader>tf", ":ToggleTerm size=70 direction=float<CR>", { desc = "Toggle floatting terminal" }),
+  vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" }),
 }
