8f0a6a2233be25351c97eac9ec5accd04ecd3cc0
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2026-02-04 14:24:31 +0100

    feat: add lazygit plugin and update toggleterm to use lazygit

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,14 +1,40 @@
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
-  config = true,
   opts = {
     config = function()
       require("toggleterm").setup({})
     end,
   },
-  vim.keymap.set("n", "<leader>th", ":ToggleTerm size=20 direction=horizontal<CR>", { desc = "Toggle horizontal terminal" }),
+  config = function()
+    local Terminal = require('toggleterm.terminal').Terminal
+    local lazygit  = Terminal:new({
+      cmd = "lazygit",
+      dir = "git_dir",
+      direction = "float",
+      float_opts = {
+        border = "double",
+      },
+      -- function to run on opening the terminal
+      on_open = function(term)
+        vim.cmd("startinsert!")
+        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+      end,
+      -- function to run on closing the terminal
+      on_close = function(term)
+        vim.cmd("startinsert!")
+      end,
+    })
+
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+
+    vim.api.nvim_set_keymap("n", "<leader>tg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true, desc = "Toggle lazygit terminal" })
+  end,
+  vim.keymap.set("n", "<leader>th", ":ToggleTerm size=20 direction=horizontal<CR>",
+    { desc = "Toggle horizontal terminal" }),
   vim.keymap.set("n", "<leader>tv", ":ToggleTerm size=70 direction=vertical<CR>", { desc = "Toggle vertical terminal" }),
   vim.keymap.set("n", "<leader>tf", ":ToggleTerm size=70 direction=float<CR>", { desc = "Toggle floatting terminal" }),
-  vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" }),
+  -- vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" }),
 }
