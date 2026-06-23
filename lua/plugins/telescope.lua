a9bedd5b044c96853427024ca73826d4f9b5658c
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2026-06-23 10:05:05 +0200

    feat: add nvim telescope-live-grep-args to telescope plugins and bind keymap for it

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,12 +2,18 @@ return {
   {
     'nvim-telescope/telescope.nvim',
     branch = "master",
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim', {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      -- This will not install any breaking changes.
+      -- For major updates, this must be adjusted manually.
+      version = "^1.0.0",
+    }, },
     config = function()
       local builtin = require("telescope.builtin")
       -- keymaps
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
       vim.keymap.set('n', '<leader>lg', builtin.live_grep, { desc = "Live grep" })
+      vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", {desc = "Live grep args"})
     end
   },
 
